c74ff6acf5a0544be86e43c9546dcd9ecd85f5df
Julian Bilcke <julian.bilcke@huggingface.co>
2023-08-28 10:41:20 +0200

    hmpf

--- src/lib/dirtyLLMResponseCleaner.ts
+++ src/lib/dirtyLLMResponseCleaner.ts
@@ -1,8 +1,14 @@
 export function dirtyLLMResponseCleaner(input: string) {
   return (
     `${input || ""}`
+    // a summary of all the weird hallucinations I saw it make..
     .replaceAll("}}", "}")
     .replaceAll("]]", "]")
     .replaceAll(",,", ",")
+    .replaceAll("[0]", "")
+    .replaceAll("[1]", "")
+    .replaceAll("[2]", "")
+    .replaceAll("[3]", "")
+    .replaceAll("[4]", "")
   )
 }
