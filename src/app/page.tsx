60d3d5eab844482411e3129c2e117e503963e284
Julian Bilcke <julian.bilcke@huggingface.co>
2023-10-25 13:55:15 +0200

    maintenance in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Head from "next/head"
 import Main from "./main"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import Script from "next/script"
+import { cn } from "@/lib/utils"
+import { fonts } from "@/lib/fonts"
 
 // https://nextjs.org/docs/pages/building-your-application/optimizing/fonts 
 
@@ -21,7 +23,22 @@ export default async function IndexPage({ params: { ownerId } }: { params: { own
          bg-zinc-50 text-stone-900 overflow-y-scroll
         `}>
         <TooltipProvider delayDuration={100}>
-          <Main />
+          
+          {/*<Main />*/}
+          
+          <div className="z-20 fixed inset-0 w-screen h-screen bg-white text-stone-800 flex flex-col items-center justify-center">
+            <div className={cn(
+              fonts.actionman.className,
+              "text-center"
+            )}>
+              <p className="text-4xl">🚧 Maintenance in progress 🚧</p>
+              <p className="text-3xl mt-12 mb-8">See the <a
+                href="https://huggingface.co/spaces/jbilcke-hf/ai-comic-factory/discussions/339"
+                className="underline text-yellow-500"
+                >announcement here</a> <img src="/quick-and-dirty-emoji.png" className="inline w-10 h-10"></img></p>
+              <p className="text-2xl">This shouldn&apos;t last long, so stay tuned!</p>
+            </div>
+          </div>
         </TooltipProvider>
         <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-WH4MGSHS" />
         <Script id="google-analytics">
