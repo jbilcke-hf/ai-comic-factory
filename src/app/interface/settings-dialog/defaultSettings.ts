42aea6a7ce4dd139b2ffd620df3d0aa48551a387
Julian Bilcke <julian.bilcke@huggingface.co>
2023-11-07 17:18:46 +0100

    improvement to the UX

--- src/app/interface/settings-dialog/defaultSettings.ts
+++ src/app/interface/settings-dialog/defaultSettings.ts
@@ -2,13 +2,13 @@ import { RenderingModelVendor, Settings } from "@/types"
 
 export const defaultSettings: Settings = {
   renderingModelVendor: "SERVER" as RenderingModelVendor,
-  huggingfaceApiKey: "<USE YOUR OWN TOKEN>",
+  huggingfaceApiKey: "",
   huggingfaceInferenceApiModel: "stabilityai/stable-diffusion-xl-base-1.0",
   huggingfaceInferenceApiModelTrigger: "",
-  replicateApiKey: "<USE YOUR OWN TOKEN>",
+  replicateApiKey: "",
   replicateApiModel: "stabilityai/sdxl",
   replicateApiModelVersion: "da77bc59ee60423279fd632efb4795ab731d9e3ca9705ef3341091fb989b7eaf",
   replicateApiModelTrigger: "style of TOK",
-  openaiApiKey: "<USE YOUR OWN TOKEN>",
+  openaiApiKey: "",
   openaiApiModel: "dall-e-3",
 }
