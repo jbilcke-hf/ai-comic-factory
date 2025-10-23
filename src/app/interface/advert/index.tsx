1c8157464b4052b1b669893f224591e61edcac2e
Julian Bilcke <julian.bilcke@huggingface.co>
2025-10-23 19:22:11 +0200

    change label

--- src/app/interface/advert/index.tsx
+++ src/app/interface/advert/index.tsx
@@ -6,9 +6,9 @@ export function Advert() {
       variant="outline"
       className="bg-yellow-400 border-stone-600/30 hover:bg-yellow-300"
       onClick={() => {
-        window.open("https://huggingface.co/spaces/jbilcke-hf/FacePoke", "_blank")
+        window.open("https://huggingface.co/spaces/jbilcke-hf/AiComicFactory2", "_blank")
       }}>
-      <span className="hidden md:inline">Try my new space</span>
+      <span className="hidden md:inline">Try the V2!</span>
       <span className="inline md:hidden">...</span>
     </Button>
   )
